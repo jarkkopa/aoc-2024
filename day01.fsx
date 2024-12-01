cf53cfdc3d05537cc9030a852bb0750b2241b286
Jarkko Palokangas <hpmandito@gmail.com>
2024-12-01 17:03:01 +0200

    Add day 1 part 2

--- day01.fsx
+++ day01.fsx
@@ -2,16 +2,23 @@ let input = "inputs/day01.txt" |> System.IO.File.ReadAllLines
 
 let split (withStr: string)(a: string) = a.Split withStr
 
-let dist (a,b) =
-    a-b
-    |> abs
+let splitAndUnzip =
+    Array.map (split "  " >> (Array.map int) >> (fun x -> (Array.head x, Array.last x)))
+    >> Array.unzip
 
-let part1 =
-    input
-    |> Array.map (split "  " >> (Array.map int) >> (fun x -> (Array.head x, Array.last x)))
-    |> Array.unzip
+// Part 1
+input
+    |> splitAndUnzip
     |> fun (a, b) -> (Array.sort a, Array.sort b)
-    |> fun (a, b) -> Array.zip a b
-    |> Array.sumBy dist
+    ||> Array.zip
+    |> Array.sumBy (fun (a, b) -> abs (a - b))
+    |> printfn "Part one: %A"
 
-part1 |> printfn "Part one: %A"
+let lengthBy pred = Array.filter pred >> Array.length
+
+// Part 2
+input
+    |> splitAndUnzip
+    |> fun (fstArr, sndArr) -> fstArr |> Array.map (fun a -> (a, sndArr |> lengthBy (fun x -> x = a)))
+    |> Array.sumBy (fun (a, b) -> a * b)
+    |> printfn "Part two: %A"
