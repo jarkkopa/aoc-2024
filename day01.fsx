85fd1d5303c72348a6f9fcea094a02b03207eb7d
Jarkko Palokangas <hpmandito@gmail.com>
2024-12-01 15:59:42 +0200

    Clean up

--- day01.fsx
+++ day01.fsx
@@ -8,12 +8,10 @@ let dist (a,b) =
 
 let part1 =
     input
-    |> Array.map (split "  ")
-    |> Array.map (Array.map int)
-    |> Array.map (fun x -> (Array.head x, Array.last x))
+    |> Array.map (split "  " >> (Array.map int) >> (fun x -> (Array.head x, Array.last x)))
     |> Array.unzip
-    |> (fun x -> (x |> fst |> Array.sort, x |> snd |> Array.sort))
-    |> (fun x -> Array.zip (fst x) (snd x))
+    |> fun (a, b) -> (Array.sort a, Array.sort b)
+    |> fun (a, b) -> Array.zip a b
     |> Array.sumBy dist
 
 part1 |> printfn "Part one: %A"
